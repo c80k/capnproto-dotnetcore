32873501f5da6cca21b7e5f46e0cb00dc542b3aa
Christian Köllner <christian.koellner.80@gmail.com>
2019-07-14 19:41:13 +0200

    Choco deployment fixed

--- chocolatey/capnpc-csharp-win-x86/chocolateyinstall.ps1
+++ chocolatey/capnpc-csharp-win-x86/chocolateyinstall.ps1
@@ -2,5 +2,4 @@ $ErrorActionPreference = 'Stop'
 
 $toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 
-choco install capnproto
 Install-Binfile -Name capnpc-csharp -Path "$toolsDir\capnpc-csharp.exe"
--- scripts/capnpc-csharp-pack.ps1
+++ scripts/capnpc-csharp-pack.ps1
@@ -8,5 +8,5 @@ $csprojFile = "capnpc-csharp.csproj"
 dotnet publish -c Release -r win-x86 --self-contained -o "$chocoDir\$id_win_x86\bin" "$csprojDir\$csprojFile"
 dotnet publish -c Release -o "$chocoDir\$id\bin" "$csprojDir\$csprojFile"
 
-choco pack "$chocoDir\$id\$id.nuspec" --outputdirectory "$chocoDir\$id\bin"
-choco pack "$chocoDir\$id_win_x86\$id_win_x86.nuspec" --outputdirectory "$chocoDir\$id_win_x86\bin"
+choco pack "$chocoDir\$id\$id.nuspec" --outputdirectory "$chocoDir\install"
+choco pack "$chocoDir\$id_win_x86\$id_win_x86.nuspec" --outputdirectory "$chocoDir\install"
