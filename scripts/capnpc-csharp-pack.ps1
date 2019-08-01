af5c9eb44469c7d1e05fb167bdc4a8d4ab15439e
Christian Köllner <christian.koellner.80@gmail.com>
2019-08-01 19:09:48 +0200

    Corrections according to Chocolatey review

--- scripts/capnpc-csharp-pack.ps1
+++ scripts/capnpc-csharp-pack.ps1
@@ -4,9 +4,16 @@ $scriptDir = Split-Path -Parent $MyInvocation.MyCommand.Path
 $chocoDir = "$scriptDir\..\chocolatey"
 $csprojDir = "$scriptDir\..\capnpc-csharp"
 $csprojFile = "capnpc-csharp.csproj"
+$installDir = "$chocoDir\install"
 
+dotnet build -c Release "$scriptDir\..\Capnp.Net.sln"
 dotnet publish -c Release -r win-x86 --self-contained -o "$chocoDir\$id_win_x86\bin" "$csprojDir\$csprojFile"
 dotnet publish -c Release -o "$chocoDir\$id\bin" "$csprojDir\$csprojFile"
 
-choco pack "$chocoDir\$id\$id.nuspec" --outputdirectory "$chocoDir\install"
-choco pack "$chocoDir\$id_win_x86\$id_win_x86.nuspec" --outputdirectory "$chocoDir\install"
+If(!(test-path $installDir))
+{
+      New-Item -ItemType Directory -Force -Path $installDir
+}
+
+choco pack "$chocoDir\$id\$id.nuspec" --outputdirectory $installDir
+choco pack "$chocoDir\$id_win_x86\$id_win_x86.nuspec" --outputdirectory $installDir
