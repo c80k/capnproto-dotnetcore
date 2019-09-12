d34d3b72a75731142a16ecf47516f1dff42fdc58
Christian Köllner <christian.koellner.80@gmail.com>
2019-09-12 22:08:00 +0200

    Adapted compile-test.bat to new file naming

--- install-test/compile-test.bat
+++ install-test/compile-test.bat
@@ -1,8 +1,8 @@
 @echo off
 capnp compile -ocsharp -I%cd% .\capnp\schema.capnp
 if %ERRORLEVEL% neq 0 exit /b 1
-if exist .\capnp\schema.cs (    
-  del .\capnp\schema.cs
+if exist .\capnp\schema.capnp.cs (    
+  del .\capnp\schema.capnp.cs
 ) else (
   exit /b 2
 )
