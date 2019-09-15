16346a7743e837d51c97f0fcc9340af3ec337a83
Christian Köllner <christian.koellner.80@gmail.com>
2019-09-15 21:09:15 +0200

    Fix casing problem

--- Capnpc.Csharp.MsBuild.Generation/ICapnpCsharpGenerator.cs
+++
@@ -1,9 +0,0 @@
-﻿using System.Collections.Generic;
-
-namespace CapnpC.CSharp.MsBuild.Generation
-{
-    public interface ICapnpcCsharpGenerator
-    {
-        IEnumerable<string> GenerateFilesForProject(string projectPath, List<CapnpGenJob> jobs, string projectFolder);
-    }
-}
