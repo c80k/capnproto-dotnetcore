b6c84c3c6c3ad48ef8953e7db4c2f0a758c9bb15
Christian Köllner <christian.koellner.80@gmail.com>
2020-03-21 14:40:46 +0100

    reorg file structure

--- scripts/measure-coverage.ps1
+++ scripts/measure-coverage.ps1
@@ -5,11 +5,8 @@ $coverageReportDir = "$rootDir\coverage\report"
 $openCover = "$env:LOCALAPPDATA\Apps\OpenCover\OpenCover.Console.exe"
 $vsTestConsole = where.exe vstest.console
 
-$runtimeTestsDnc21 = "$rootDir\Capnp.Net.Runtime.Tests.Core21\bin\Release\netcoreapp2.1\Capnp.Net.Runtime.Tests.Core21.dll"
-$coverageOutputRuntimeDnc21 = "$coverageDir\cov-Capnp.Net.Runtime-dnc21.xml"
-
-$runtimeTestsNet471 = "$rootDir\Capnp.Net.Runtime.Tests\bin\Release\net471\Capnp.Net.Runtime.Tests.Std20.dll"
-$coverageOutputRuntimeNet471 = "$coverageDir\cov-Capnp.Net.Runtime-net471.xml"
+$runtimeTests = "$rootDir\Capnp.Net.Runtime.Tests\bin\Release\netcoreapp2.1\Capnp.Net.Runtime.Tests.dll"
+$coverageOutputRuntime = "$coverageDir\cov-Capnp.Net.Runtime.xml"
 
 $generatorTests = "$rootDir\CapnpC.CSharp.Generator.Tests\bin\Release\netcoreapp3.0\CapnpC.CSharp.Generator.Tests.dll"
 $coverageOutputGenerator = "$coverageDir\cov-CapnpC.CSharp.Generator.xml"
@@ -25,17 +22,10 @@ If(!(test-path $coverageReportDir))
 }
 
 & $openCover -target:"$vsTestConsole" `
-  -targetArgs:"/inIsolation $runtimeTestsDnc21 /TestCaseFilter:`"TestCategory=Coverage`"" `
+  -targetArgs:"/inIsolation $runtimeTests /TestCaseFilter:`"TestCategory=Coverage`"" `
   -filter:"+[Capnp.Net.Runtime]Capnp.*" `
   -excludebyattribute:"System.CodeDom.Compiler.GeneratedCodeAttribute" `
-  -output:"$coverageOutputRuntimeDnc21" `
-  -mergebyhash -register:user -oldStyle
-
-& $openCover -target:"$vsTestConsole" `
-  -targetArgs:"/inIsolation $runtimeTestsNet471 /TestCaseFilter:`"TestCategory=Coverage`"" `
-  -filter:"+[Capnp.Net.Runtime]Capnp.*" `
-  -excludebyattribute:"System.CodeDom.Compiler.GeneratedCodeAttribute" `
-  -output:"$coverageOutputRuntimeNet471" `
+  -output:"$coverageOutputRuntime" `
   -mergebyhash -register:user -oldStyle
 
 & $openCover -target:"$vsTestConsole" `
@@ -45,4 +35,4 @@ If(!(test-path $coverageReportDir))
   -output:"$coverageOutputGenerator" `
   -mergebyhash -register:user -oldStyle
 
-ReportGenerator.exe -reports:"$coverageOutputRuntimeDnc21;$coverageOutputRuntimeNet471;$coverageOutputGenerator" -targetdir:"$coverageReportDir" -reportTypes:"Html;Xml"
+ReportGenerator.exe -reports:"$coverageOutputRuntime;$coverageOutputGenerator" -targetdir:"$coverageReportDir" -reportTypes:"Html;Xml"
